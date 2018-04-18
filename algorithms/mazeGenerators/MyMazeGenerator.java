b51f50a136cee82c5215ae9380f8ddd0e9e9a022
liron refaeli <lironref@post.bgu.ac.il>
2018-04-18 16:40:17 +0300

    myMazeGenerator with javaDoc- finished

--- algorithms/mazeGenerators/MyMazeGenerator.java
+++ algorithms/mazeGenerators/MyMazeGenerator.java
@@ -6,66 +6,224 @@ import java.util.Random;
 import java.util.Stack;
 
 
-
+/**
+ * This class represents a DFS maze generator.
+ * The depth-first search algorithm of maze generation is frequently implemented using backtracking.
+ * @since 18.4.18
+ */
 public class MyMazeGenerator extends AMazeGenerator {
 
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
         IMazeGenerator myMazeGenerator = new MyMazeGenerator();
          long estimatedTime = myMazeGenerator.measureAlgorithmTimeMillis(1000, 1000);
          System.out.println(estimatedTime);
         Maze maze = myMazeGenerator.generate(10,10);
         maze.print();
     }
+    */
 
-    @Override
-    public Maze generate(int numOfRows, int numOfColumns) {
-
-        Position startPosition = getRandomStartPosition(numOfRows);
-        Position goalPosition=getRandomGoalPosition(numOfColumns);
-        Maze m_maze=new Maze(numOfRows,numOfColumns,startPosition,goalPosition);
-        generatingPath(m_maze);
-        generateRandomly(m_maze);
-        return m_maze;
-    }
-
-    private void generateRandomly(Maze maze) {
-        for (int i = 0; i < maze.getM_rows(); i++) {
-            for (int j = 0; j < maze.getM_columns(); j++) {
-                if (maze.getM_maze()[i][j] != 0) {
-                    if (Math.random() < 0.2)
-                        maze.getM_maze()[i][j] = 0;
-                    else
-                        maze.getM_maze()[i][j] = 1;
-                }
-            }
-        }
-
-    }
-
-    private Position getRandomStartPosition(int numOfRows)
-    {
+    /**
+     * This method gets the num of rows of the maze, and return random
+     * position which is updated as the maze's start position.
+     * @param numOfRows
+     * @return start Position
+     */
+    private Position getRandomStartPosition(int numOfRows) {
         Random rand = new Random();
         int randomStart= rand.nextInt(numOfRows-1);
         Position startPosition=new Position(randomStart,0);
         return startPosition;
 
     }
-
-    private Position getRandomGoalPosition(int numOfColumns)
-    {
+    /**
+     * This method gets the num of columns and rows of the maze, and return random
+     * position which is updated as the maze's goal position.
+     * @param numOfColumns
+     * @param numOfRows
+     * @return goal Position
+     */
+    private Position getRandomGoalPosition(int numOfRows,int numOfColumns) {
         Random rand = new Random();
-        int randomGoal= rand.nextInt(numOfColumns-1);
+        int randomGoal= rand.nextInt(numOfRows-1);
         Position goalPosition=new Position(randomGoal,numOfColumns-1);
         return goalPosition;
     }
 
+    /**
+     * This method gets a maze and position(num of row and column)
+     * and return if the cell in the maze is free(the value:0), or if it's a wall(the value:1)
+     * @param maze
+     * @param row
+     * @param col
+     * @return 0 for free cell, 1 for a wall
+     */
     private int getCellValue(Maze maze, int row, int col)
     {
         return maze.getM_maze()[row][col];
     }
 
-    private void generatingPath(Maze maze)
-    {
+    /**
+     * This method checks if a given position(row and column) is out of the bounds of the maze.
+     * @param row
+     * @param col
+     * @param maze
+     * @return true for a cell which is out of bound.
+     */
+    private boolean isOutOfBounds(int row, int col, Maze maze) {
+        return (row < 0 || col < 0 || row >= maze.getM_rows()|| col >= maze.getM_columns());
+    }
+
+    /**
+     * This method return a list of positions which are neighbors of the current given position,
+     * aren't a wall cells, and their location is in the maze's bounds.
+     * This list is important for choosing random neighbor to develop the maze's path from the
+     * current position to him.
+     * @param maze
+     * @param current
+     * @return a list of positions
+     */
+    private List<Position> findNeighbors(Maze maze,Position current) {
+        List<Position> neighborsList = new ArrayList<>();
+        boolean isAvailable;
+
+        if (!isOutOfBounds(current.getRowIndex(), current.getColumnIndex() + 1, maze)) {
+            isAvailable=true;
+            if (maze.getM_maze()[current.getRowIndex()][current.getColumnIndex() + 1] == 1) {
+
+                if(!isOutOfBounds(current.getRowIndex()-1, current.getColumnIndex() + 1, maze))
+                {
+                    if(getCellValue(maze,current.getRowIndex()-1, current.getColumnIndex() + 1 )!=1)
+                        isAvailable=false;
+                }
+
+                if(isAvailable && !isOutOfBounds(current.getRowIndex()+1, current.getColumnIndex() + 1, maze))
+                {
+                    if(getCellValue(maze,current.getRowIndex()+1, current.getColumnIndex() + 1 )!=1)
+                        isAvailable=false;
+                }
+
+                if(isAvailable && !isOutOfBounds(current.getRowIndex(), current.getColumnIndex() + 2, maze))
+                {
+                    if(getCellValue(maze,current.getRowIndex(), current.getColumnIndex() + 2 )!=1)
+                        isAvailable=false;
+                }
+
+                if(isAvailable) {
+                    Position p = new Position(current.getRowIndex(), current.getColumnIndex() + 1);
+                    neighborsList.add(p);
+                }
+
+            }
+        }
+
+
+        if (!isOutOfBounds(current.getRowIndex() - 1, current.getColumnIndex(), maze)) {
+            isAvailable=true;
+
+            if (maze.getM_maze()[current.getRowIndex() - 1][current.getColumnIndex()] == 1) {
+
+                if(!isOutOfBounds(current.getRowIndex()-1, current.getColumnIndex() + 1, maze))
+                {
+                    if(getCellValue(maze,current.getRowIndex()-1, current.getColumnIndex() + 1 )!=1)
+                        isAvailable=false;
+                }
+
+                if(isAvailable && !isOutOfBounds(current.getRowIndex()-1, current.getColumnIndex() - 1, maze))
+                {
+                    if(getCellValue(maze,current.getRowIndex()-1, current.getColumnIndex() - 1 )!=1)
+                        isAvailable=false;
+                }
+
+                if(isAvailable && !isOutOfBounds(current.getRowIndex()-2, current.getColumnIndex() , maze))
+                {
+                    if(getCellValue(maze,current.getRowIndex()-2, current.getColumnIndex() )!=1)
+                        isAvailable=false;
+                }
+
+                if(isAvailable) {
+                    Position p = new Position(current.getRowIndex() - 1, current.getColumnIndex());
+                    neighborsList.add(p);
+                }
+            }
+        }
+
+
+
+        if (!isOutOfBounds(current.getRowIndex() + 1, current.getColumnIndex(), maze)) {
+            isAvailable=true;
+
+            if (maze.getM_maze()[current.getRowIndex() + 1][current.getColumnIndex()] == 1) {
+
+                if(!isOutOfBounds(current.getRowIndex()+1, current.getColumnIndex()-1 , maze))
+                {
+                    if(getCellValue(maze,current.getRowIndex()+1, current.getColumnIndex()-1 )!=1)
+                        isAvailable=false;
+                }
+
+                if(isAvailable && !isOutOfBounds(current.getRowIndex()+1, current.getColumnIndex()+1 , maze))
+                {
+                    if(getCellValue(maze,current.getRowIndex()+1, current.getColumnIndex()+1 )!=1)
+                        isAvailable=false;
+                }
+
+                if(isAvailable && !isOutOfBounds(current.getRowIndex()+2, current.getColumnIndex() , maze))
+                {
+                    if(getCellValue(maze,current.getRowIndex()+2, current.getColumnIndex() )!=1)
+                        isAvailable=false;
+                }
+                if(isAvailable) {
+                    Position p = new Position(current.getRowIndex() + 1, current.getColumnIndex());
+                    neighborsList.add(p);
+                }
+            }
+        }
+
+
+        if (!isOutOfBounds(current.getRowIndex(), current.getColumnIndex() - 1, maze)) {
+            isAvailable=true;
+            if (maze.getM_maze()[current.getRowIndex()][current.getColumnIndex() - 1] == 1) {
+
+                if(!isOutOfBounds(current.getRowIndex()-1, current.getColumnIndex()-1 , maze))
+                {
+                    if(getCellValue(maze,current.getRowIndex()-1, current.getColumnIndex()-1 )!=1)
+                        isAvailable=false;
+
+                }
+
+                if(isAvailable && !isOutOfBounds(current.getRowIndex()+1, current.getColumnIndex()-1 , maze))
+                {
+                    if(getCellValue(maze,current.getRowIndex()+1, current.getColumnIndex()-1 )!=1)
+                        isAvailable=false;
+
+                }
+
+                if(!isOutOfBounds(current.getRowIndex(), current.getColumnIndex()-2 , maze))
+                {
+                    if(getCellValue(maze,current.getRowIndex(), current.getColumnIndex()-2 )!=1)
+                        isAvailable=false;
+
+                }
+
+
+                if(isAvailable) {
+                    Position p = new Position(current.getRowIndex(), current.getColumnIndex() - 1);
+                    neighborsList.add(p);
+                }
+            }
+        }
+
+
+
+
+        return neighborsList;
+    }
+
+    /**
+     * This method is generating the path of the maze from the start position to the
+     * goal position. the generation is done by DFS algorithm. The neighbors are chosen randomly.
+     * @param maze
+     */
+    private void generatingPath(Maze maze) {
         Position current=maze.getStartPosition();
         maze.getM_maze()[current.getRowIndex()][current.getColumnIndex()] = 0;
 
@@ -112,143 +270,44 @@ public class MyMazeGenerator extends AMazeGenerator {
 
     }
 
-    private List<Position> findNeighbors(Maze maze,Position current)
-    {
-        List<Position> neighborsList = new ArrayList<>();
-        boolean isAvailable;
-
-            if (!isOutOfBounds(current.getRowIndex(), current.getColumnIndex() + 1, maze)) {
-                isAvailable=true;
-                if (maze.getM_maze()[current.getRowIndex()][current.getColumnIndex() + 1] == 1) {
-
-                    if(!isOutOfBounds(current.getRowIndex()-1, current.getColumnIndex() + 1, maze))
-                    {
-                        if(getCellValue(maze,current.getRowIndex()-1, current.getColumnIndex() + 1 )!=1)
-                            isAvailable=false;
-                    }
-
-                    if(isAvailable && !isOutOfBounds(current.getRowIndex()+1, current.getColumnIndex() + 1, maze))
-                    {
-                        if(getCellValue(maze,current.getRowIndex()+1, current.getColumnIndex() + 1 )!=1)
-                            isAvailable=false;
-                    }
-
-                    if(isAvailable && !isOutOfBounds(current.getRowIndex(), current.getColumnIndex() + 2, maze))
-                    {
-                        if(getCellValue(maze,current.getRowIndex(), current.getColumnIndex() + 2 )!=1)
-                            isAvailable=false;
-                    }
-
-                    if(isAvailable) {
-                        Position p = new Position(current.getRowIndex(), current.getColumnIndex() + 1);
-                        neighborsList.add(p);
-                    }
-
+    /**
+     * This method is spreading randomly 0 and 1 on the blocked cells in the maze,
+     * in order to make the maze more difficult to solve.
+     * @param maze
+     */
+    private void generateRandomly(Maze maze) {
+        for (int i = 0; i < maze.getM_rows(); i++) {
+            for (int j = 0; j < maze.getM_columns(); j++) {
+                if (maze.getM_maze()[i][j] != 0) {
+                    if (Math.random() < 0.2)
+                        maze.getM_maze()[i][j] = 0;
+                    else
+                        maze.getM_maze()[i][j] = 1;
                 }
             }
+        }
 
-
-            if (!isOutOfBounds(current.getRowIndex() - 1, current.getColumnIndex(), maze)) {
-               isAvailable=true;
-
-                if (maze.getM_maze()[current.getRowIndex() - 1][current.getColumnIndex()] == 1) {
-
-                    if(!isOutOfBounds(current.getRowIndex()-1, current.getColumnIndex() + 1, maze))
-                    {
-                        if(getCellValue(maze,current.getRowIndex()-1, current.getColumnIndex() + 1 )!=1)
-                            isAvailable=false;
-                    }
-
-                    if(isAvailable && !isOutOfBounds(current.getRowIndex()-1, current.getColumnIndex() - 1, maze))
-                    {
-                        if(getCellValue(maze,current.getRowIndex()-1, current.getColumnIndex() - 1 )!=1)
-                            isAvailable=false;
-                    }
-
-                    if(isAvailable && !isOutOfBounds(current.getRowIndex()-2, current.getColumnIndex() , maze))
-                    {
-                        if(getCellValue(maze,current.getRowIndex()-2, current.getColumnIndex() )!=1)
-                            isAvailable=false;
-                    }
-
-                    if(isAvailable) {
-                        Position p = new Position(current.getRowIndex() - 1, current.getColumnIndex());
-                        neighborsList.add(p);
-                    }
-                }
-            }
-
-
-
-            if (!isOutOfBounds(current.getRowIndex() + 1, current.getColumnIndex(), maze)) {
-                isAvailable=true;
-
-                if (maze.getM_maze()[current.getRowIndex() + 1][current.getColumnIndex()] == 1) {
-
-                    if(!isOutOfBounds(current.getRowIndex()+1, current.getColumnIndex()-1 , maze))
-                    {
-                        if(getCellValue(maze,current.getRowIndex()+1, current.getColumnIndex()-1 )!=1)
-                            isAvailable=false;
-                    }
-
-                    if(isAvailable && !isOutOfBounds(current.getRowIndex()+1, current.getColumnIndex()+1 , maze))
-                    {
-                        if(getCellValue(maze,current.getRowIndex()+1, current.getColumnIndex()+1 )!=1)
-                            isAvailable=false;
-                    }
-
-                    if(isAvailable && !isOutOfBounds(current.getRowIndex()+2, current.getColumnIndex() , maze))
-                    {
-                        if(getCellValue(maze,current.getRowIndex()+2, current.getColumnIndex() )!=1)
-                            isAvailable=false;
-                    }
-                   if(isAvailable) {
-                       Position p = new Position(current.getRowIndex() + 1, current.getColumnIndex());
-                       neighborsList.add(p);
-                   }
-                }
-            }
-
-
-            if (!isOutOfBounds(current.getRowIndex(), current.getColumnIndex() - 1, maze)) {
-               isAvailable=true;
-                if (maze.getM_maze()[current.getRowIndex()][current.getColumnIndex() - 1] == 1) {
-
-                    if(!isOutOfBounds(current.getRowIndex()-1, current.getColumnIndex()-1 , maze))
-                    {
-                        if(getCellValue(maze,current.getRowIndex()-1, current.getColumnIndex()-1 )!=1)
-                            isAvailable=false;
-
-                    }
-
-                    if(isAvailable && !isOutOfBounds(current.getRowIndex()+1, current.getColumnIndex()-1 , maze))
-                    {
-                        if(getCellValue(maze,current.getRowIndex()+1, current.getColumnIndex()-1 )!=1)
-                            isAvailable=false;
-
-                    }
-
-                    if(!isOutOfBounds(current.getRowIndex(), current.getColumnIndex()-2 , maze))
-                    {
-                        if(getCellValue(maze,current.getRowIndex(), current.getColumnIndex()-2 )!=1)
-                            isAvailable=false;
-
-                    }
-
-
-                   if(isAvailable) {
-                       Position p = new Position(current.getRowIndex(), current.getColumnIndex() - 1);
-                       neighborsList.add(p);
-                   }
-                }
-            }
-
-
-
-
-        return neighborsList;
     }
-    private boolean isOutOfBounds(int row, int col, Maze maze) {
-        return (row < 0 || col < 0 || row >= maze.getM_rows()|| col >= maze.getM_columns());
+
+
+    /**
+     *  This method gets num of rows and num of columns and return a maze
+     *  In the method start position and goal position are choosen randomly.
+     *  The maze includes a solution path from the start position to the end position.
+     *  The path was generated by the DFS algorithm.
+     * @param numOfRows
+     * @param numOfColumns
+     * @return maze
+     */
+    public Maze generate(int numOfRows, int numOfColumns) {
+
+        Position startPosition = getRandomStartPosition(numOfRows);
+        Position goalPosition=getRandomGoalPosition(numOfRows,numOfColumns);
+        Maze m_maze=new Maze(numOfRows,numOfColumns,startPosition,goalPosition);
+        generatingPath(m_maze);
+        generateRandomly(m_maze);
+        return m_maze;
     }
+
+
 }
